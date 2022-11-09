be7f9682038042126a47903d396083c88f332a24
Thomas-rnd <75882329+Thomas-rnd@users.noreply.github.com>
2022-11-09 15:07:06 +0100

    Probleme 3

--- Class1.cs
+++ Class1.cs
@@ -62,42 +62,135 @@ namespace ProjetIA2022
         public override double CalculeHCost()
         {
 
-            //x et y du noeaud examiné
+             //x et y du noeaud examiné
             //Form1.xinitial et Form1.yinitial qui sont accessible car static
             //Form1.xfinal Form1.yfinal
             //matrice[x,y] indique le type de case (-1 si marécage, 0 si rien, -2 si obstacle) 
 
             //Problème 1 : 
 
-            return (Math.Sqrt((x - Form1.xfinal) ^ 2 + (y - Form1.yfinal) ^ 2));
-
+            //return (Math.Sqrt((this.x - Form1.xfinal)*(this.x - Form1.xfinal) + (this.y - Form1.yfinal)*(this.y - Form1.yfinal)));
+            
             //Problème 2 : Décomposition en 2 sous problèmes en passant par un point de passage
+            /*
+            Node2 pointPassage = new Node2();
+            pointPassage.x = 10;
+            pointPassage.y = 8;
 
-            Node2 pointPassage = new Node2(10, 20);
-
-            if (x < 10)
-                return (Math.Sqrt((x - pointPassage.x) ^ 2 + (y - pointPassage.y) ^ 2));
-            else
-                return (Math.Sqrt((x - Form1.xfinal) ^ 2 + (y - Form1.yfinal) ^ 2));
-
-            //Problème 3 : 
-            Node2 pointInterdit1 = new Node2(1, 1);
-            Node2 pointInterdit2 = new Node2(1, 1);
-            //Devant l'entrée interdite
-            Node2 pointPassage1 = new Node2(1, 1);
-            //En haut
-            Node2 pointPassage2 = new Node2(10, 20);
-            if (x < 10)
+            bool PI= true; // false si gauche true si droite
+            bool PF= true; // false si gauche true si droite
+            if (x <= 10)
             {
-                if (x != pointInterdit1.x && x != pointInterdit2.x && y != pointInterdit1.y && y != pointInterdit2.y)
-                    return (Math.Sqrt((x - pointPassage1.x) ^ 2 + (y - pointPassage1.y) ^ 2));
-                else
-                    return (1000);
+                PI= false;
+            }
+
+            if (Form1.xfinal <= 10)
+            {
+                PF= false;
+            }
+            // Test si du même côté
+            if (PI != PF)
+            {
+                return (Math.Abs(x - pointPassage.x) + Math.Abs(y - pointPassage.y) + Math.Abs(pointPassage.x - Form1.xfinal) + Math.Abs(pointPassage.y - Form1.yfinal));
             }
             else
-                return (Math.Sqrt((x - Form1.xfinal) ^ 2 + (y - Form1.yfinal) ^ 2));
+            {
+                return (Math.Abs(x - Form1.xfinal) + Math.Abs(y - Form1.yfinal));
+            }
+            */
 
-            return (0);
+            
+            //Problème 3 : 
+
+            bool PI= true; // false si gauche true si droite
+            bool PF= true; // false si gauche true si droite
+
+            //Test même côté
+            if (x <= 10)
+            {
+                PI= false;
+            }
+
+            if (Form1.xfinal <= 10)
+            {
+                PF= false;
+            }
+
+            Node2 pointPassage = new Node2();
+            pointPassage.x = 10;
+            pointPassage.y = 0;
+
+            Node2 pointPassageCercle = new Node2();
+            pointPassageCercle.x = 2;
+            pointPassageCercle.y = 6;
+
+            double h1=Math.Abs(x - Form1.xfinal) + Math.Abs(y - Form1.yfinal);// Distance point actuel/point final
+            double h2=Math.Abs(x - pointPassage.x) + Math.Abs(y - pointPassage.y);// Distance point actuel/point passage
+            double h3=Math.Abs(pointPassageCercle.x - x) + Math.Abs(pointPassageCercle.y - y);// Distance point actuel/point passage cercle
+            double h4=Math.Abs(pointPassageCercle.x - pointPassage.x) + Math.Abs(pointPassageCercle.y - pointPassage.y);// Distance point passage cercle/ point passage
+            double h5=Math.Abs(Form1.xfinal - pointPassage.x) + Math.Abs(Form1.yfinal - pointPassage.y);// Distance point passage/point final
+            double h6=Math.Abs(Form1.xfinal - pointPassageCercle.x) + Math.Abs(Form1.yfinal - pointPassageCercle.y);// Distance point passage cerlce/point final
+
+            //Test si points de départ et arrivée dans le cercle
+            //Point initial dans le cercle
+            if((x>=3 && x<=8) && (y>=4 && y<=9))
+            {
+                if((Form1.xfinal>=3 && Form1.xfinal<=8) && (Form1.yfinal >=4 && Form1.yfinal <=9))
+                {
+                     return h1;
+                }
+                else
+                {
+                    // côtés différents
+                    if (PI != PF)
+                    {
+                        return  h3 + h4 + h5;
+                    }
+                    //Même coté
+                    else
+                    {
+                        return h3 + h6;
+                    }
+                }
+            }
+            // Point initial en dehors du cercle
+            else
+            {
+                if((Form1.xfinal>=3 && Form1.xfinal<=8) && (Form1.yfinal >=4 && Form1.yfinal <=9))
+                {
+                     if (PI != PF)
+                    {
+                        return  h2 + h4 + h6;
+                    }
+                    //Même coté
+                    else
+                    {
+                        return h3 + h6;
+                    }
+                }
+                else
+                {
+                    // côtés différents
+                    if (PI != PF)
+                    {
+                        if(Form1.yinitial>=6 && Form1.yfinal<6)
+                        {
+                            return h2+h3+h5;
+                        }
+                        return  h2+h5;
+                    }
+                    //Même coté
+                    else
+                    {
+                        if(Form1.yinitial>=6 && Form1.yfinal < 6)
+                        {
+                            return h1+h3;
+                        }
+                        return h1;
+                    }
+                }
+            }
+        }
 
         }
 
