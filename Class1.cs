a1a1462c7791a336cf52b6b2873a3a0f59b0a973
Thomas RENAUD <renaudthomas@orange.fr>
2022-11-11 17:32:10 +0100

    Version finale

--- Class1.cs
+++ Class1.cs
@@ -68,29 +68,32 @@ namespace ProjetIA2022
 
             //Problème 1 : 
 
-            //return (Math.Sqrt((this.x - Form1.xfinal)*(this.x - Form1.xfinal) + (this.y - Form1.yfinal)*(this.y - Form1.yfinal)));
-            
+            //return (Math.Abs(this.x - Form1.xfinal) + Maths.Abs(this.y - Form1.yfinal);
+
             //Problème 2 : Décomposition en 2 sous problèmes en passant par un point de passage
             /*
-            Node2 pointPassage = new Node2();
-            pointPassage.x = 10;
-            pointPassage.y = 8;
+            Node2 pointPassageFrontiere = new Node2();
+            pointPassageFrontiere.x = 10;
+            pointPassageFrontiere.y = 8;
 
-            bool PI= true; // false si gauche true si droite
-            bool PF= true; // false si gauche true si droite
+            bool PCCote = true; // Le point courant est du côté droit
+            bool PFCote = true; // Le point final est du côté droit
+
+            // Initialisation PCCote
             if (x <= 10)
             {
-                PI= false;
+                PCCote = false;
             }
-
+            // Initialisation PFCote
             if (Form1.xfinal <= 10)
             {
-                PF= false;
+                PFCote = false;
             }
+
             // Test si du même côté
-            if (PI != PF)
+            if (PCCote != PFCote)
             {
-                return (Math.Abs(x - pointPassage.x) + Math.Abs(y - pointPassage.y) + Math.Abs(pointPassage.x - Form1.xfinal) + Math.Abs(pointPassage.y - Form1.yfinal));
+                return (Math.Abs(x - pointPassageFrontiere.x) + Math.Abs(y - pointPassageFrontiere.y) + Math.Abs(pointPassageFrontiere.x - Form1.xfinal) + Math.Abs(pointPassageFrontiere.y - Form1.yfinal));
             }
             else
             {
@@ -98,128 +101,159 @@ namespace ProjetIA2022
             }
             */
 
-            
+
             //Problème 3 : 
 
-            bool  PCCercle = true; // Le point courant est dans le cercle
-            bool  PFCercle = true; // Le point final est dans le cercle
+            bool PCCercle = true; // Le point courant est dans le cercle
+            bool PFCercle = true; // Le point final est dans le cercle
 
-            bool PCCote= true; // Le point courant est du côté droit
-            bool PFCote= true; // Le point final est du côté droit
+            bool PCCote = true; // Le point courant est du côté droit
+            bool PFCote = true; // Le point final est du côté droit
 
-            bool  PFMarecages = true; // Le point est dans le marécage
-            bool  PCMarecages = true; // Le point est dans le marécage
-           
+            bool PFMarecages = true; // Le point est dans le marécage
+            bool PCMarecages = true; // Le point est dans le marécage
+
             //Initialisation PCCercle
-            if(!((x>=3 && x<=8) && (y>=4 && y<=9)))
+            if (!(x >= 3 && x <= 8) || !(y >= 4 && y <= 9))
             {
                 PCCercle = false;
             }
             //Initialisation PFCercle
-            if(!((Form1.xfinal>=3 && Form1.xfinal<=8) && (Form1.yfinal>=4 && Form1.yfinal<=9)))
+            if (!(Form1.xfinal >= 3 && Form1.xfinal <= 8) || !(Form1.yfinal >= 4 && Form1.yfinal <= 9))
             {
                 PFCercle = false;
             }
-            
+
             // Initialisation PCCote
             if (x <= 10)
             {
-                PCCote= false;
+                PCCote = false;
             }
             // Initialisation PFCote
             if (Form1.xfinal <= 10)
             {
-                PFCote= false;
+                PFCote = false;
             }
 
             //Initialisation PFMarecages
-            if(Form1.yfinal >= 10)
+            if (Form1.yfinal >= 10)
             {
-                PFMarecages= false; 
+                PFMarecages = false;
             }
             //Initialisation PCMarecages
-            if(y >= 10)
+            if (y >= 10)
             {
-                PCMarecages= false; 
+                PCMarecages = false;
             }
 
+            // Définition du point de passage obligatoire si on veut changer de côté
             Node2 pointPassageFrontiere = new Node2();
             pointPassageFrontiere.x = 10;
             pointPassageFrontiere.y = 0;
 
+            // Défintion du point de passage obligatoire si on veut entrer ou sortir du cercle 
             Node2 pointPassageCercle = new Node2();
             pointPassageCercle.x = 2;
             pointPassageCercle.y = 6;
 
-            double h1=Math.Abs(x - Form1.xfinal) + Math.Abs(y - Form1.yfinal);// Distance point actuel/point final
-            double h2=Math.Abs(x - pointPassageFrontiere.x) + Math.Abs(y - pointPassageFrontiere.y);// Distance point actuel/point passage
-            double h3=Math.Abs(pointPassageCercle.x - x) + Math.Abs(pointPassageCercle.y - y);// Distance point actuel/point passage cercle
-            double h4=Math.Abs(pointPassageCercle.x - pointPassageFrontiere.x) + Math.Abs(pointPassageCercle.y - pointPassageFrontiere.y);// Distance point passage cercle/ point passage
-            double h5=Math.Abs(Form1.xfinal - pointPassageFrontiere.x) + Math.Abs(Form1.yfinal - pointPassageFrontiere.y);// Distance point passage/point final
-            double h6=Math.Abs(Form1.xfinal - pointPassageCercle.x) + Math.Abs(Form1.yfinal - pointPassageCercle.y);// Distance point passage cerlce/point final
+            // Défintion d'un point de sorti du marégage
+            Node2 pointPassageMarecage = new Node2();
+            pointPassageMarecage.x = 11;
+            pointPassageMarecage.y = 10;
 
-            
+            // Distance point courant/point final
+            double h1 = Math.Abs(x - Form1.xfinal) + Math.Abs(y - Form1.yfinal);
+            // Distance point courant/point passage frontière
+            double h2 = Math.Abs(x - pointPassageFrontiere.x) + Math.Abs(y - pointPassageFrontiere.y);
+            // Distance point courant/point passage cercle
+            double h3 = Math.Abs(pointPassageCercle.x - x) + Math.Abs(pointPassageCercle.y - y);
+            // Distance point passage cercle/ point passage frontière
+            double h4 = Math.Abs(pointPassageCercle.x - pointPassageFrontiere.x) + Math.Abs(pointPassageCercle.y - pointPassageFrontiere.y);
+            // Distance point passage frontière/point final
+            double h5 = Math.Abs(Form1.xfinal - pointPassageFrontiere.x) + Math.Abs(Form1.yfinal - pointPassageFrontiere.y);
+            // Distance point passage cerlce/point final
+            double h6 = Math.Abs(Form1.xfinal - pointPassageCercle.x) + Math.Abs(Form1.yfinal - pointPassageCercle.y);
+            // Distance point passage frontière/point passage marécage
+            double h7 = Math.Abs(pointPassageMarecage.x - pointPassageFrontiere.x) + Math.Abs(pointPassageMarecage.y - pointPassageFrontiere.y);
+            // Distance point passage marécage/point final
+            double h8 = Math.Abs(pointPassageMarecage.x - Form1.xfinal) + Math.Abs(pointPassageMarecage.y - Form1.yfinal);
+            // Distance point courant/point passage marécage
+            double h9 = Math.Abs(pointPassageMarecage.x - x) + Math.Abs(pointPassageMarecage.y - y);
+
+            // Creation d'un dictionnaire associant une distance particulière à sa valeur.
+            Dictionary<string, double> distanceH = new Dictionary<string, double>();
+            distanceH.Add("PCtoPF", h1);
+            distanceH.Add("PCtoPPF", h2);
+            distanceH.Add("PCtoPPC", h3);
+            distanceH.Add("PPCtoPPF", h4);
+            distanceH.Add("PPFtoPF", h5);
+            distanceH.Add("PPCtoPF", h6);
+            distanceH.Add("PPFtoPSM", h7);
+            distanceH.Add("PPMtoPF", h8);
+            distanceH.Add("PCtoPPM", h9);
+
+
             //PC dans le cercle
-            if(PCCercle)
+            if (PCCercle)
             {
                 //PF dans le cercle
-                if(PFCercle)
+                if (PFCercle)
                 {
-                    return(h1);
+                    return (distanceH["PCtoPF"]);
                 }
                 //PF en dehors du cercle
                 else
                 {
                     //PF et PC du même côté
-                    if(PFCote=PCCote)
+                    if (PFCote = PCCote)
                     {
-                        return(h3+h6);
+                        return (distanceH["PCtoPPC"] + distanceH["PPCtoPF"]);
                     }
                     //PF/PC côté différent
                     else
                     {
                         //Marécages
-                        if(PFMarecages)
+                        if (PFMarecages)
                         {
                             //Si le point est dans le marécage le coût du trajet
-                            //final entre PC et PF est multiplié par 3 comme pour
+                            //entre PPF et PF est multiplié par 3 comme pour
                             //le trajet réel.
-                            return(h3+h4+3*h1);
+                            return (distanceH["PCtoPPC"] + distanceH["PPCtoPPF"] + 3 * distanceH["PPFtoPF"]);
                         }
-                        //Non marécages
+                        //PF est situé en dessous du marégage
                         else
                         {
-                            return(h3+h4+3*h1);
+                            return (distanceH["PCtoPPC"] + distanceH["PPCtoPPF"] + 3 * distanceH["PPFtoPPM"] + distanceH["PPMtoPF"]);
                         }
                     }
                 }
             }
             //PC pas dans le cercle
             else
-            { 
+            {
                 //PF dans le cercle 
-                if(PFCercle)
-                { 
+                if (PFCercle)
+                {
                     //PF et PC même côté
-                    if(PFCote=PCCote)
-                    { 
-                        return(h3+h1);
+                    if (PFCote = PCCote)
+                    {
+                        return (distanceH["PCtoPPC"] + distanceH["PPCtoPF"]);
                     }
                     //PF/PC côté différent
                     else
                     {
                         //Marécages
-                        if(PCMarecages)
+                        if (PCMarecages)
                         {
                             //Si le point est dans le marécage le coût du trajet
-                            //final entre PC et PF est multiplié par 3 comme pour
+                            //entre PPF et PF est multiplié par 3 comme pour
                             //le trajet réel.
-                            return(3*h2+h4+h1);
+                            return (3 * distanceH["PCtoPPF"] + distanceH["PPFtoPPC"] + distanceH["PPCtoPF"]);
                         }
                         //Non marécages
                         else
                         {
-                            return(3*h2+h4+h1);
+                            return (distanceH["PCtoPPM"] + 3 * distanceH["PPMtoPPF"] + distanceH["PPFtoPPC"] + distanceH["PPCtoPF"]);
                         }
                     }
                 }
@@ -227,54 +261,114 @@ namespace ProjetIA2022
                 else
                 {
                     //PF et PC même côté
-                    if(PFCote=PCCote)
+                    if (PFCote = PCCote)
                     {
-                        //Contourner Cercle ?
-                        if(!PFCote)
+                        // Dans le cas où les points sont du côté gauche
+                        // On va regarder s'il est necessaire de contourner le cercle
+                        if (!PFCote)
                         {
-                            if(y>6 && Form1.yfinal<6 || y<6 && Form1.yfinal>6)
+                            if ((y > 6 && Form1.yfinal < 6) || (y < 6 && Form1.yfinal > 6))
                             {
-                                return(h3+h6);
+                                return (distanceH["PCtoPPC"] + distanceH["PPCtoPF"]);
                             }
                             else
                             {
-                                return(h1);
+                                return (distanceH["PCtoPF"]);
                             }
                         }
-                        //Marécages ? 
+                        // Dans le cas où les points sont du côté droit
+                        // On va regarder où ils sont placés par rapport au marécage
                         else
                         {
-                            return(3*h1);
+                            //PC et PF dans le marécage
+                            if (PCMarecages && PFMarecages)
+                            {
+                                return (3 * distanceH["PCtoPF"]);
+                            }
+                            //Un des deux en dehors
+                            else if (PCMarecages != PFMarecages)
+                            {
+                                // C'est PC qui est dans le marécage
+                                if (PCMarecages)
+                                {
+                                    return (3 * distanceH["PCtoPPM"] + distanceH["PPMtoPF"]);
+                                }
+                                // C'est PF qui est dans le marécage
+                                else
+                                {
+                                    return (distanceH["PCtoPPM"] + 3 * distanceH["PPMtoPF"]);
+                                }
+                            }
                         }
                     }
                     //PF/PC côté différent 
                     else
                     {
-                        if(!PFCote)
+                        //PFCercle est à gauche donc PC à droite
+                        if (!PFCote)
                         {
-                            //Contourner Cercle 
-                            if(Form1.yfinal>6)
+                            //On doit contourner le cercle 
+                            if (Form1.yfinal > 6)
                             {
-                                return(3*h2+h4+h6);
+                                // PC est dans le marécage
+                                if (PCMarecages)
+                                {
+                                    return (3 * distanceH["PCtoPPF"] + distanceH["PPFtoPPC"] + distanceH["PPCtoPF"]);
+                                }
+                                // PC est en dessous du marécage
+                                else
+                                {
+                                    return (distanceH["PCtoPPM"] + 3 * distanceH["PPMtoPPF"] + distanceH["PPFtoPPC"] + distanceH["PPCtoPF"]);
+                                }
                             }
+                            //On ne doit contourner le cercle 
                             else
                             {
-                                return(3*h2+h5);
+                                // PC est dans le marécage
+                                if (PCMarecages)
+                                {
+                                    return (3 * distanceH["PCtoPPF"] + distanceH["PPFtoPF"]);
+                                }
+                                // PC est en dessous du marécage
+                                else
+                                {
+                                    return (distanceH["PCtoPPM"] + 3 * distanceH["PPMtoPPF"] + distanceH["PPFtoPF"]);
+                                }
                             }
                         }
+                        //PFCercle est à droite donc PC à gauche
                         else
                         {
-                            //Contourner Cercle 
-                            if(y>6)
+                            //On doit contourner Cercle 
+                            if (y > 6)
                             {
-                                return(h3+h4+3*h5);
+                                // PF est dans le marécage
+                                if (PFMarecages)
+                                {
+                                    return (distanceH["PCtoPPC"] + distanceH["PPCtoPPF"] + 3 * distanceH["PPFtoPF"]);
+                                }
+                                // PF est en dessous du marécage
+                                else
+                                {
+                                    return (distanceH["PCtoPPC"] + distanceH["PPCtoPPF"] + 3 * distanceH["PPFtoPPM"] + distanceH["PPMtoPF"]);
+                                }
                             }
+                            // On ne doit pas contourner le cercle
                             else
                             {
-                                return(h2+3*h5);
+                                // PF est dans le marécage
+                                if (PFMarecages)
+                                {
+                                    return (distanceH["PCtoPPF"] + 3 * distanceH["PPFtoPF"]);
+                                }
+                                // PF est en dessous du marécage
+                                else
+                                {
+                                    return (distanceH["PCtoPPF"] + 3 * distanceH["PPFtoPPM"] + distanceH["PPMtoPF"]);
+                                }
                             }
                         }
-                    } 
+                    }
                 }
             }
         }
