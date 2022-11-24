611ea1e19337fa3c59ecc7855e99d92d4667b39f
Thomas RENAUD <renaudthomas@orange.fr>
2022-11-24 18:05:06 +0100

    Hcost1

--- HCost1.cs
+++ HCost1.cs
@@ -35,46 +35,67 @@
 //la distance de Manhattan entre 2 points.
 
 //Distance Manhattan entre PC et PF (un nombre de cases)
+<<<<<<< Updated upstream
 double dist = DistReel(x,y,Form1.xfinal,Form1.yfinal); 
 
 //Compteur du nombre de marécage rencontré sur le chemin
 int cmptMarecages =0;
 int cmptTotal = 1;
+=======
+double distManhattan = Math.Abs(this.x - Form1.xfinal) + Math.Abs(this.y - Form1.yfinal);
+
+//Compteur du nombre de marécage rencontré sur le chemin
+int cmpt = 0;
+>>>>>>> Stashed changes
 
 //Variable intermédiare pour ne pas modifier les coordonnées du point courant
 int cheminX = x;
 int cheminY = y;
+<<<<<<< Updated upstream
 while(cheminX!=Form1.xfinal || cheminY!=Form1.yfinal)
 {
     cmptTotal+=1;
     if(cheminX<Form1.xfinal)
+=======
+while (cheminX != Form1.xfinal && cheminY != Form1.yfinal)
+{
+    if (cheminX < Form1.xfinal)
+>>>>>>> Stashed changes
     {
-        cheminX+=1;      
+        cheminX += 1;
     }
-    else if(cheminX>Form1.xfinal)
+    else if (cheminX > Form1.xfinal)
     {
-        cheminX-=1;
-    }
-
-    if(cheminY<Form1.yfinal)
-    {
-        cheminY+=1;
-    }
-    else if(cheminY>Form1.yfinal)
-    {
-        cheminY-=1;
+        cheminX -= 1;
     }
 
+    if (cheminY < Form1.yfinal)
+    {
+        cheminY += 1;
+    }
+    else if (cheminY > Form1.yfinal)
+    {
+        cheminY -= 1;
+    }
+
+<<<<<<< Updated upstream
     if (Form1.matrice[cheminX, cheminY] == -1)
     {
        cmptMarecages+=1;
     }
+=======
+    if (matrice[cheminX, cheminY] == -1) ; cmpt += 1;
+>>>>>>> Stashed changes
 }
 
 //La distance totale est alors la somme du nombre de marécage rencontré fois trois
 //plus le reste des cases à parcourir pour arriver à PF
+<<<<<<< Updated upstream
 double distUnitaire = (double) dist/cmptTotal;
 return(dist + 2*(cmptMarecages/cmptTotal)*dist);
+=======
+return (3 * cmpt + (distManhattan - cmpt));
+>>>>>>> Stashed changes
 
 //Enoncer le problème si on est en haut à gauche et bas à droite du marécage
 //Chemin le plus court on sort du marécage donc vu que l'estimation passe par
